0db47138cab20b450499f35f5499d13af113c1f1
Jack Kieny <jackkieny@cox.net>
2025-02-03 14:41:55 -0600

    feat: add handler function to parse time from ISO to cron format

---
+++ handlers/handlers.go
@@ -0,0 +1,26 @@
+package handlers
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/rs/zerolog/log"
+)
+
+func ParseTime(isoTime string) (string, error) {
+	layout := "2006-01-02 15:04:05.000 -0700 MST"
+	t, err := time.Parse(layout, isoTime)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to parse time")
+	}
+
+	minute := t.Minute()
+	hour := t.Hour()
+	day := t.Day()
+	month := t.Month()
+	weekday := t.Weekday()
+
+	cron := fmt.Sprintf("%d %d %d %d %d", minute, hour, day, month, weekday)
+	fmt.Println(cron)
+	return "", nil
+}
